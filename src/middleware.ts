4c1ad1693b96f0ef06c5a070bec12a91991c9d59
shamelali <shamelali@gmail.com>
2026-07-30 12:54:58 +0800

    fix(csp): add unsafe-eval for script-src to support third-party scripts

--- src/middleware.ts
+++ src/middleware.ts
@@ -65,7 +65,7 @@ function withSecurityHeaders(res: NextResponse, nonce: string) {
   res.headers.set("x-nonce", nonce);
   res.headers.set(
     "Content-Security-Policy",
-    `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self'; connect-src 'self' https://cloudflareinsights.com https://api.cloudinary.com; frame-src 'none'; object-src 'none'`,
+    `default-src 'self'; script-src 'self' 'unsafe-eval' 'nonce-${nonce}' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self'; connect-src 'self' https://cloudflareinsights.com https://api.cloudinary.com; frame-src 'none'; object-src 'none'`,
   );
   return res;
 }
